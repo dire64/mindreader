5298eca86e35ef0c0856dd0ff60578629b65dba0
darkmatter222 <dash222345@gmail.com>
2024-03-28 09:23:50 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -102,3 +102,14 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 });
+
+function toggleChat() {
+    var chatBox = document.getElementById('chat-box');
+    var displayStatus = chatBox.style.display;
+    
+    if(displayStatus === 'none') {
+      chatBox.style.display = 'block';
+    } else {
+      chatBox.style.display = 'none';
+    }
+  }
