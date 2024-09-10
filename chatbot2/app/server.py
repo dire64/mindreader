ac65a3c7974571ddade2e8935e15916b8e0eb509
Jacob Rotundo <rotundja@sheridancollege.ca>
2024-09-10 12:38:33 -0400

    Integrated chatbot2 with website
    Integrated the new chatbot endpoint with our website and also hide the chat button when the chatbox is visible.

---
+++ chatbot2/app/server.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langchain.prompts import ChatPromptTemplate
+from fastapi.middleware.cors import CORSMiddleware
+from langchain.chat_models import ChatOpenAI
+from langserve import add_routes
+
+trainingString = """Pretend you are a mental health professional who can only 
+    answer mental health related inquires. Using 2-3 sentences, give advice based 
+    on the following input if you are unable to provide adequite help, refer users to 
+    our website has resources to book an appointment with a therapist or the forum to 
+    talk to other people who can help with mental health: """
+
+app = FastAPI(
+    title = "Mental Health Chatbot",
+    version = "0.1",
+    description = "Mental health chatbot that gives only mental health advice."
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+model = ChatOpenAI(model = "gpt-3.5-turbo-1106", openai_api_key = "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", temperature = 1.0, max_tokens = 100)
+prompt = ChatPromptTemplate.from_template(trainingString + "{message}")
+
+add_routes(
+    app,
+    prompt | model,
+    path="/chat"
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
