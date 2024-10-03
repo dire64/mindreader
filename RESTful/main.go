a9ed0295ffcdf7358e586d74448b9fb81a369934
dire64 <64617300+dire64@users.noreply.github.com>
2024-10-03 15:50:58 -0400

    Added Postgres integration and Swagger Documentation
    The RESTful API now integrates with Postgres and we can add connection information later when setting up our webserver, also added Swagger Documentation

--- RESTful/main.go
+++ RESTful/main.go
@@ -1,49 +1,179 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 	"strings"
 
+	_ "mindreader/web-service/docs"
+
+	_ "github.com/lib/pq"
+
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-// struct that represents a professional and their data.
-type professional struct {
-	ID      string `json:"id"`
-	Name    string `json:"name"`
-	Address string `json:"address"`
-	City    string `json:"city"`
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!!MOVE TO A FILE THAT IS GITIGNORED LATER!!!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1234"
+	dbname   = "postgres"
+)
+
+var db *sql.DB
+
+// Initalize the database.
+func ConnectDatabase() {
+	var err error
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal("[POSTGRES] Failed to open database:", err)
+	}
+
+	// Check if the database is reachable
+	if err := db.Ping(); err != nil {
+		log.Fatal("[POSTGRES] Failed to connect to the database:", err)
+	}
+
+	fmt.Println("[POSTGRES] Successfully connected to database!")
 }
 
-// temporary data, and stuff.
-var professionals = []professional{
-	{ID: "1", Name: "Mr. Gopher", Address: "123 Main Street", City: "Brampton"},
-	{ID: "2", Name: "Ms. Gopher", Address: "456 Doctor Road", City: "Toronto"},
-}
-
+// @title           MindReader Doctor API
+// @version         1.0
+// @description     API for retreiving professionals used on MindReader.
 func main() {
 	router := gin.Default()
+	ConnectDatabase()
+	defer db.Close()
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.GET("/professionals", getProfessionals)
 	router.GET("/professionals/:city", getProfessionalsInCity)
 
 	router.Run("localhost:8000")
 }
 
+// @Summary Get Professionals
+// @Description Get all available professionals.
+// @Tags professionals
+// @Accept json
+// @Produce json
+// @Success 200 {object} professional
+// @Failure 404 {object} ErrorResponse
+// @Router /professionals/ [get]
 // get all the professionals in the database as JSON.
 func getProfessionals(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+
+	rows, err := db.QueryContext(c, "SELECT * FROM professionals")
+	if err != nil {
+		errorResponse := ErrorResponse{
+			Code:    http.StatusInternalServerError,
+			Message: err.Error(),
+		}
+		c.JSON(http.StatusInternalServerError, errorResponse)
+		panic(err)
+	}
+	defer rows.Close()
+
+	var professionals []professional
+	for rows.Next() {
+		var p professional
+		err := rows.Scan(&p.Id, &p.Name, &p.Address, &p.City)
+		if err != nil {
+			errorResponse := ErrorResponse{
+				Code:    http.StatusInternalServerError,
+				Message: err.Error(),
+			}
+			c.JSON(http.StatusInternalServerError, errorResponse)
+		}
+		professionals = append(professionals, p)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		errorResponse := ErrorResponse{
+			Code:    http.StatusInternalServerError,
+			Message: err.Error(),
+		}
+		c.JSON(http.StatusInternalServerError, errorResponse)
+	}
+
 	c.IndentedJSON(http.StatusOK, professionals)
 }
 
+// @Summary Get Professionals by City
+// @Description Get all available professionals in a given city.
+// @Tags professionals
+// @Accept json
+// @Produce json
+// @Param city path string true "City"
+// @Success 200 {array} professional
+// @Failure 404 {object} ErrorResponse
+// @Router /professionals/{city} [get]
 func getProfessionalsInCity(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+
 	city := strings.ToLower(c.Param("city"))
+	query := "SELECT * FROM professionals WHERE LOWER(city) = LOWER($1)"
 
-	var profs []professional
-
-	for _, professional := range professionals {
-		if strings.ToLower(professional.City) == city {
-			profs = append(profs, professional)
+	rows, err := db.QueryContext(c, query, city)
+	if err != nil {
+		errorResponse := ErrorResponse{
+			Code:    http.StatusInternalServerError,
+			Message: err.Error(),
 		}
+		c.JSON(http.StatusInternalServerError, errorResponse)
+		panic(err)
+	}
+	defer rows.Close()
+
+	var professionals []professional
+	for rows.Next() {
+		var p professional
+		err := rows.Scan(&p.Id, &p.Name, &p.Address, &p.City)
+		if err != nil {
+			errorResponse := ErrorResponse{
+				Code:    http.StatusInternalServerError,
+				Message: err.Error(),
+			}
+			c.JSON(http.StatusInternalServerError, errorResponse)
+		}
+		professionals = append(professionals, p)
 	}
 
-	c.IndentedJSON(http.StatusOK, profs)
+	err = rows.Err()
+	if err != nil {
+		errorResponse := ErrorResponse{
+			Code:    http.StatusInternalServerError,
+			Message: err.Error(),
+		}
+		c.JSON(http.StatusInternalServerError, errorResponse)
+	}
+
+	c.IndentedJSON(http.StatusOK, professionals)
+}
+
+// struct that responds to errors on endpoints.
+type ErrorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+// struct that represents a professional and their data.
+type professional struct {
+	Id      string `json:"id"`
+	Name    string `json:"name"`
+	Address string `json:"address"`
+	City    string `json:"city"`
 }
